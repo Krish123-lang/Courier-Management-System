b165ba08f4ac9e61f44c4b0c341b48a010c85936
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2026-07-06 00:14:01 +0545

    Delivery Assignment

--- customer/admin.py
+++ customer/admin.py
@@ -6,10 +6,32 @@ from .models import *
 admin.site.register(Invoice)
 admin.site.register(PaymentMethod)
 admin.site.register(ShipmentTracking)
-admin.site.register(DeliveryAssignment)
+
+
+class DeliveryAssignmentInline(admin.TabularInline):
+    model = DeliveryAssignment
+    extra = 1
+    readonly_fields = ("assigned_at",)
+    fields = ("delivery_staff", "status", "assigned_by", "assigned_at")
+
 
 class ShipmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "trackingId", "sender_name", "recipientName", "delivery_status")
+    list_display = ("id", "trackingId", "sender_name", "recipientName", "delivery_status", "assigned_delivery_staff")
+    inlines = [DeliveryAssignmentInline]
+
+    def assigned_delivery_staff(self, obj):
+        latest = obj.assignments.order_by("-assigned_at").first()
+        return latest.delivery_staff.email if latest and latest.delivery_staff else "-"
+    assigned_delivery_staff.short_description = "Delivery Staff"
+
+
+class DeliveryAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("shipment", "delivery_staff", "assigned_by", "status", "assigned_at")
+    list_filter = ("status", "delivery_staff")
+    search_fields = ("shipment__trackingId", "delivery_staff__email")
+
+
+admin.site.register(DeliveryAssignment, DeliveryAssignmentAdmin)
 admin.site.register(Shipment, ShipmentAdmin)
 
 
